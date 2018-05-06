94e433e98f34bfcd127c17d4fef2823f3d984286
honeydlp <2747622419@qq.com>
2018-05-06 21:23:28 +0800

    test

--- audioSelf/tools.js
+++ audioSelf/tools.js
@@ -8,7 +8,8 @@ export function sec_to_time(value,markOpts){
 	var secondTime = parseInt(value,10);// 秒
 	var minuteTime = 0;// 分
 	var hourTime = 0;// 小时
-	if(secondTime > 59) {//如果秒数大于60，将秒数转换成整数
+	
+	if(secondTime > 59) {//如果秒数大于60，将秒数转换成   整数
 		//获取分钟，除以60取整数，得到整数分钟
 		minuteTime = parseInt(secondTime / 60,10);
 		//获取秒数，秒数取佘，得到整数秒数
